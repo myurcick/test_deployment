f9a9d9f18fb67cfdefe2995d29d47bbc934abee6
myurcick <166910242+myurcick@users.noreply.github.com>
2025-10-06 13:43:13 +0300

    Update Program.cs

--- backend/Backend/Program.cs
+++ backend/Backend/Program.cs
@@ -50,7 +50,7 @@ builder.Services.AddSwaggerGen(c =>
 
 // EF Core
 var conn = builder.Configuration.GetConnectionString("DefaultConnection") ??
-           "Server=profkomlnu-server;Database=profkomdb;Uid=seavotgupm;Pwd=DBkN9Ww8Lra$jKjC;";
+           "hostname=profkomlnu-server.mysql.database.azure.com;port=3306;username=seavotgupm;password=DBkN9Ww8Lra$jKjC;";
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(conn, ServerVersion.AutoDetect(conn))
